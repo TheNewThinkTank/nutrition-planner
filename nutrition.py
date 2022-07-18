dba5623f85b9cd072e11ac757dd5c3cddd5dfe7d
Gustav Rasmussen <gcr84@hotmail.com>
2022-07-18 23:47:30 +0200

    chore: get dataframes

--- nutrition.py
+++ nutrition.py
@@ -1,11 +1,14 @@
 """_summary_
 """
 
+import pandas as pd
+
 # from pprint import pprint as pp
 
-from nutrition_facts import nutrition_facts_100g
+# from nutrition_facts import nutrition_facts_100g
 
 
+"""
 class Meal:
     def __init__(self, ingredients: dict) -> None:
         self.ingredients = {
@@ -20,20 +23,46 @@ class Meal:
         return all_facts
 
     def get_macros(self):
-        pass
+        carbs = ""
+        fat = ""
+        protein = ""
+
+        return carbs, fat, protein
 
     def get_micros(self):
-        pass
+
+        fat_unsaturated = ""
+        fiber = ""
+        amino_acids = ""
+        minerals = ""
+        vitamins = ""
+
+        return fat_unsaturated, fiber, amino_acids, minerals, vitamins
+"""
 
 
 def main():
     """_summary_"""
 
-    dinner = Meal({"Broccoli": "100g", "Chicken breast": "300g", "Brown rice": "180g"})
-    all_facts = dinner.get_all_facts()
-    print(all_facts)
-    dinner.get_macros()
-    dinner.get_micros()
+    snack = {}
+    breakfast = {}
+    lunch = {}
+    dinner = {"Broccoli": "100g", "Chicken breast": "300g", "Brown rice": "180g"}
+    cheat_meal = {}
+
+    # dinner = Meal({"Broccoli": "100g", "Chicken breast": "300g", "Brown rice": "180g"})
+    # all_facts = dinner.get_all_facts()
+    # print(all_facts)
+    # dinner.get_macros()
+    # dinner.get_micros()
+
+    def get_df(ingredient):
+        return pd.read_csv(
+            f"data/{ingredient}.csv", skiprows=range(6), skipfooter=4, engine="python"
+        )
+
+    df_broccoli = get_df("broccoli_raw")
+    print(df_broccoli)
 
 
 if __name__ == "__main__":
