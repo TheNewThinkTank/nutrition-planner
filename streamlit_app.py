f0839c79e3691624982b7971530b32ba284d7cb0
Gustav Rasmussen <gcr84@hotmail.com>
2022-07-24 18:58:32 +0200

    feat: add get_facts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 """_summary_
 """
 
+import requests
 import streamlit as st
 
 st.title("Nutrition App")
@@ -12,8 +13,29 @@ Created by Gustav C. Rasmussen. Powered by nutritionix
 """
 )
 
-# dish = st.radio("Pick a dish", ["succotash", "curried pumpkin soup"])
+ingredient = st.sidebar.text_input("Ingredient")
+amount = st.sidebar.text_input("Amount")
+unit = st.sidebar.text_input("Unit")
 
-st.sidebar.text_input("Ingredient")
-st.sidebar.text_input("Amount")
-st.sidebar.text_input("Unit")
+
+URL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+
+NUTRITIONIX_ID = st.secrets["NUTRITIONIX_ID"]
+NUTRITIONIX_KEY = st.secrets["NUTRITIONIX_KEY"]
+
+HEADER = "Content-Type: application/json,"
+f"x-app-id: {NUTRITIONIX_ID},"
+f"x-app-key: {NUTRITIONIX_KEY}"
+
+BODY = {"query": f"{amount}{unit} of {ingredient}", "timezone": "US/Eastern"}
+
+
+def get_facts():
+
+    response = requests.post(
+        URL,
+        headers=HEADER,
+        json=BODY,
+    )
+
+    return response.text
