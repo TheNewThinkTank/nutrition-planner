4de1c82fa901bf9a6a91e7a9aa5090c3411caba2
Gustav Rasmussen <gcr84@hotmail.com>
2022-07-25 17:56:24 +0200

    feat: add wedgeprops to pie chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,7 @@ ax1.pie(
     labels=labels,
     colors=colors,
     autopct="%1.1f%%",
+    wedgeprops={"edgecolor": "black", "linewidth": 2, "antialiased": True},
 )
 ax1.axis("equal")
 st.pyplot(fig1)
