adecbb64e3fd3c65a34f0ddfe1db546c87ee3f50
Gustav Rasmussen <gcr84@hotmail.com>
2022-08-10 23:38:05 +0200

    feat: skip dark_background

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,6 @@ st.write(
     f"{time.time() - start_time:.1f} seconds"
 )
 
-plt.style.use("dark_background")
 fig1, ax1 = plt.subplots()
 labels = ["protein", "fat", "carbohydrate"]
 sizes = [nutrition["protein"], nutrition["fat"], nutrition["carbs"]]
